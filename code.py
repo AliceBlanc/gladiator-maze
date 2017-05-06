848cc5016d27d54f744a76d1f82e8c5d387b4ac0
Olivier Blanc <olivier.blanc@dgfip.finances.gouv.fr>
2017-05-06 19:41:39 +0200

    Ajout de la lecture rapide du labyrinthe

--- code.py
+++ code.py
@@ -3,40 +3,68 @@ def creerCase(mur, passage, sortie, joueur, gladiateur):
     return case
 
 
-def saisirCase():
-    nature = input("Quelle est la nature de la case ? (mur, passage, sortie ou rien)").lower()
-    if nature == "mur":
-        case = creerCase(True, False, False, False, False)
-    elif nature == "passage":
-        case = creerCase(False, True, False, False, False)
-    elif nature == "sortie":
-        case = creerCase(False, False, True, False, False)
-    else:
-        personnage = input("Quel personnage est sur la case ? (joueur,  gladiateur,  aucun)").lower()
-        if personnage == "joueur":
-            case = creerCase(False, False, False, True, False)
-        elif personnage == "gladiateur":
-            case = creerCase(False, False, False, False, True)
-        else:
-            case = creerCase(False, False, False, False, False)
-    return case
+# def saisirCase():
+#     nature = input("Quelle est la nature de la case ? (mur, passage, sortie ou rien)").lower()
+#     if nature == "mur":
+#         case = creerCase(True, False, False, False, False)
+#     elif nature == "passage":
+#         case = creerCase(False, True, False, False, False)
+#     elif nature == "sortie":
+#         case = creerCase(False, False, True, False, False)
+#     else:
+#         personnage = input("Quel personnage est sur la case ? (joueur,  gladiateur,  aucun)").lower()
+#         if personnage == "joueur":
+#             case = creerCase(False, False, False, True, False)
+#         elif personnage == "gladiateur":
+#             case = creerCase(False, False, False, False, True)
+#         else:
+#             case = creerCase(False, False, False, False, False)
+#     return case
 
 
-def creerLabyrinthe(m, n):
+# def creerLabyrinthe(m, n):
+#     labyrinthe = []
+#     for i in range(0, m+1):
+#         print(i)
+#         liste = []
+#         for j in range(0, n+1):
+#             print(j)
+#             case = saisirCase()
+#             liste.append(case)
+#         labyrinthe.append(liste)
+#     return labyrinthe
+
+
+def decodeLigne(chaine):
+    ligne = []
+    for c in chaine:
+        if c == " ":
+            ligne.append(creerCase(False, False, False, False, False))
+        if c == "m":
+            ligne.append(creerCase(True, False, False, False, False))
+        if c == "p":
+            ligne.append(creerCase(False, True, False, False, False))
+        if c == "s":
+            ligne.append(creerCase(False, False, True, False, False))
+        if c == "j":
+            ligne.append(creerCase(False, False, False, True, False))
+        if c == "g":
+            ligne.append(creerCase(False, False, False, False, True))
+    return ligne
+
+
+def lireDescription():
     labyrinthe = []
-    for i in range(0, m+1):
-        print(i)
-        liste = []
-        for j in range(0, n+1):
-            print(j)
-            case = saisirCase()
-            liste.append(case)
-        labyrinthe.append(liste)
+    x = int(input("Taille X du labyrinthe :"))
+    # y = input("Taille Y du labyrinthe :")
+    for a in range(0, 2 * x + 1):
+        ligne = input("Ligne :")
+        labyrinthe.append(decodeLigne(ligne))
     return labyrinthe
 
 
 def main():
-    labyrinthe = creerLabyrinthe(1, 1)
+    labyrinthe = lireDescription()
     print(labyrinthe)
 
 
