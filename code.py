418fafc9455f7aabeca7261b74369fe5a2fd7d28
Olivier Blanc <olivier.blanc@dgfip.finances.gouv.fr>
2017-05-06 18:32:53 +0200

    Formattage

--- code.py
+++ code.py
@@ -1,38 +1,43 @@
-def creerCase(mur,passage,sortie,joueur,gladiateur):
-    case = {"mur":mur,"passage":passage,"sortie":sortie,"joueur":joueur,"gladiateur":gladiateur}
+def creerCase(mur, passage, sortie, joueur, gladiateur):
+    case = {"mur": mur, "passage": passage, "sortie": sortie, "joueur": joueur, "gladiateur": gladiateur}
     return case
+
 
 def saisirCase():
-    nature=input("Quelle est la nature de la case ? (mur, passage, sortie ou rien)").lower()            
+    nature = input("Quelle est la nature de la case ? (mur, passage, sortie ou rien)").lower()
     if nature == "mur":
-        case = creerCase(True,False,False,False,False)
+        case = creerCase(True, False, False, False, False)
     elif nature == "passage":
-        case = creerCase(False,True,False,False,False)
+        case = creerCase(False, True, False, False, False)
     elif nature == "sortie":
-        case = creerCase(False,False,True,False,False)
+        case = creerCase(False, False, True, False, False)
     else:
-        personnage=input("Quel personnage est sur la case ? (joueur, gladiateur, aucun)").lower()
+        personnage = input("Quel personnage est sur la case ? (joueur,  gladiateur,  aucun)").lower()
         if personnage == "joueur":
-            case = creerCase(False,False,False,True,False)
+            case = creerCase(False, False, False, True, False)
         elif personnage == "gladiateur":
-            case = creerCase(False,False,False,False,True)
+            case = creerCase(False, False, False, False, True)
         else:
-            case = creerCase(False,False,False,False,False)
+            case = creerCase(False, False, False, False, False)
     return case
 
-def creerLabyrinthe(m,n):
-    labyrinthe=[]
-    for i in range(0,m+1):
+
+def creerLabyrinthe(m, n):
+    labyrinthe = []
+    for i in range(0, m+1):
         print(i)
-        liste=[]
-        for j in range(0,n+1):
+        liste = []
+        for j in range(0, n+1):
             print(j)
             case = saisirCase()
             liste.append(case)
         labyrinthe.append(liste)
     return labyrinthe
 
+
 def main():
-    labyrinthe=creerLabyrinthe(1,1)
+    labyrinthe = creerLabyrinthe(1, 1)
     print(labyrinthe)
+
+
 main()
